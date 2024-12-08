4443ffabc2d749c7cb2c13f41d2628cbdb8db432
Keegan Churchill <KeeganBChurchill@gmail.com>
2024-12-07 20:52:14 -0330

    counting left and right brackets, how expressions are displayed

--- src/main.js
+++ src/main.js
@@ -1,26 +1,55 @@
-import { Parser } from 'https://cdn.jsdelivr.net/npm/expr-eval@2.0.2/+esm';
-
+import { Parser } from "https://cdn.jsdelivr.net/npm/expr-eval@2.0.2/+esm";
 
 document.addEventListener("DOMContentLoaded", function () {
-
-
   const display = document.getElementById("calc-display");
+  const entry = document.getElementById("calc-entry");
+  const answer = document.getElementById("calc-answer");
   const buttons = document.getElementsByClassName("btn");
-
-  let currentValue = "";
+  let openBracketCount = 0;
+  let emptyBracket = false;
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
       if (buttons[i].innerText == "AC") {
         display.value = "";
+        entry.value = "";
+        answer.value = "";
+        document.getElementById("close-bracket-btn").disabled = true;
+        document.getElementById("equals-btn").disabled = false;
+        openBracketCount = 0;
+        emptyBracket = false;
+      } else if (buttons[i].innerText == "CE") {
+        entry.value = "";
       } else if (buttons[i].innerText == "=") {
-        display.value = evaluateExpression(display.value);
-      } else if (buttons[i].innerText == "X") {
-        display.value += "*";
-      } else if (buttons[i].innerText == "÷") {
-        display.value += "/";
+        display.value += entry.value;
+        entry.value = "";
+        answer.value = evaluateExpression(display.value);
+      } else if (buttons[i].classList.contains("operator")) {
+        display.value += entry.value += buttons[i].innerText;
+        entry.value = "";
+      } else if (buttons[i].innerText == "(") {
+        openBracketCount++;
+        document.getElementById("equals-btn").disabled = true;
+        document.getElementById("close-bracket-btn").disabled = false;
+        if (entry.value != "") {
+          display.value += entry.value + "*" + buttons[i].innerText;
+        } else {
+          display.value += buttons[i].innerText;
+        }
+
+        entry.value = "";
+      } else if (buttons[i].innerText == ")") {
+        openBracketCount--;
+        if (openBracketCount == 0) {
+          buttons[i].disabled = true;
+          document.getElementById("equals-btn").disabled = false;
+          document.getElementById("close-bracket-btn").disabled = true;
+        }
+        display.value += entry.value + buttons[i].innerText;
+        entry.value = "";
       } else {
-        display.value += buttons[i].innerText;
+        
+        entry.value += buttons[i].innerText;
       }
     });
   }
