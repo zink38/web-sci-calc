7f96c6d573a98c191f3861a8397ae50fc0cf8958
Keegan Churchill <KeeganBChurchill@gmail.com>
2024-12-09 22:04:18 -0330

    sub percent, auto insert * when applicable

--- src/main.js
+++ src/main.js
@@ -20,14 +20,21 @@ document.addEventListener("DOMContentLoaded", function () {
         rightBracket = false;
       } else if (buttons[i].innerText == "CE") {
         entry.value = "";
-        rightBracket = false;
       } else if (buttons[i].innerText == "=") {
         display.value += entry.value;
         entry.value = "";
         rightBracket = false;
         answer.value = evaluateExpression(display.value);
       } else if (buttons[i].classList.contains("operator")) {
-        display.value += entry.value += buttons[i].innerText;
+        let rb = false;
+        if (
+          rightBracket &&
+          entry.value != ""
+        ) {
+          display.value += "*" + entry.value + buttons[i].innerText;
+        } else {
+          display.value += entry.value += buttons[i].innerText;
+        }
         if (buttons[i].innerText == "%") {
           display.value = display.value.replace(
             /(\d+)\s*\+\s*(\d+)%/,
@@ -36,15 +43,19 @@ document.addEventListener("DOMContentLoaded", function () {
             }
           );
           display.value = display.value.replace(
-            /(\d+)%/,
-            (match, percent) => {
-              return `(${percent}/100)`;
+            /(\d+)\s*\-\s*(\d+)%/,
+            (match, base, percent) => {
+              return `percentSub(${base}, ${percent})`;
             }
           );
+          display.value = display.value.replace(/(\d+)%/, (match, percent) => {
+            return `(${percent}/100)`;
+          });
+          rb = true;
         }
-        
+
         entry.value = "";
-        rightBracket = false;
+        rightBracket = rb;
       } else if (buttons[i].innerText == "(") {
         openBracketCount++;
         document.getElementById("equals-btn").disabled = true;
@@ -55,7 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
           display.value += buttons[i].innerText;
         }
         rightBracket = false;
-
         entry.value = "";
       } else if (buttons[i].innerText == ")") {
         rightBracket = true;
@@ -67,9 +77,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         display.value += entry.value + buttons[i].innerText;
         entry.value = "";
+      } else if (buttons[i].innerText == "ANS") {
+        entry.value = answer.value;
       } else {
         entry.value += buttons[i].innerText;
-        rightBracket = false;
       }
     });
   }
@@ -78,7 +89,12 @@ document.addEventListener("DOMContentLoaded", function () {
 function evaluateExpression(expression) {
   try {
     const parser = new Parser();
-    parser.functions.percentAdd = (base, percent) => { return base + (base * (percent / 100)); };
+    parser.functions.percentAdd = (base, percent) => {
+      return base + base * (percent / 100);
+    };
+    parser.functions.percentSub = (base, percent) => {
+      return base - base * (percent / 100);
+    };
     return parser.evaluate(expression);
   } catch (error) {
     return "Error";
